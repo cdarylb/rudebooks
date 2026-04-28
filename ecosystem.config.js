42b525ae35761ec4302b9e17c4d38a30e1e95515
Cyril Beaufrere <cyril.beaufrere@gmail.com>
2026-04-28 22:31:48 +0200

    chore: add PM2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'rudebooks',
+      script: 'npm',
+      args: 'start',
+      cwd: '/home/dario/apps/rudebooks',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
